05f2017e403cf2ea3ca8b533200e1cc28cc0948a
Chris Beavers <crbeavers@gmail.com>
2018-04-30 20:25:16 -0500

    Baseball PiP for xmonad

--- xmonad.hs
+++ xmonad.hs
@@ -1,4 +1,6 @@
 import XMonad
+import XMonad.Actions.CopyWindow
+import XMonad.Actions.CycleWS
 import XMonad.Actions.SpawnOn
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.Script
@@ -10,26 +12,26 @@ import System.Exit
 main = xmonad =<< xmobar defaults
 
 defaults = def
-        { manageHook = manageSpawn <+> composeAll [ className =? "onboard" --> doIgnore ] <+> manageHook def
+        { terminal = "gnome-terminal"
+        , manageHook = manageSpawn <+> composeAll [ className =? "onboard" --> doIgnore ] <+> manageHook def
         , modMask = mod4Mask
-        , workspaces = ["1","2","3","4","5","6","7","8","9","0"]
-        , startupHook = do
-            spawnOn "1" "gnome-terminal"
-            spawnOn "2" "google-chrome"
-            spawnOn "3" "slack"
-            spawnOn "9" "spotify"
+        , workspaces = ["1","2","3","4","5","6","7","8","9"]
         }
         `additionalKeysP`
-        [ ("<XF86MonBrightnessUp>", spawn "/home/chris/.xmonad/scripts/brightness.pl +")
-        , ("<XF86MonBrightnessDown>", spawn "/home/chris/.xmonad/scripts/brightness.pl -")
-        , ("<XF86AudioRaiseVolume>", spawn "amixer set Master 5+ unmute")
-        , ("<XF86AudioLowerVolume>", spawn "amixer set Master 5- unmute")
+        [ ("<XF86MonBrightnessUp>", spawn "xbacklight -inc 10")
+        , ("<XF86MonBrightnessDown>", spawn "xbacklight -dec 10")
+        , ("<XF86AudioRaiseVolume>", spawn "amixer set Master 5%+ unmute")
+        , ("<XF86AudioLowerVolume>", spawn "amixer set Master 5%- unmute")
         , ("<XF86AudioMute>", spawn "amixer -D pulse set Master toggle")
         , ("<XF86AudioPlay>", spawn "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause")
         , ("<XF86AudioPrev>", spawn "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous")
         , ("<XF86AudioNext>", spawn "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next")
-        , ("M-<Left>", spawn "/home/chris/.xmonad/scripts/rotate.sh --left")
-        , ("M-<Right>", spawn "/home/chris/.xmonad/scripts/rotate.sh --right")
-        , ("M-<Up>", spawn "/home/chris/.xmonad/scripts/rotate.sh --up")
-        , ("M-<Down>", spawn "/home/chris/.xmonad/scripts/rotate.sh --down")
+        , ("M-S-<Left>", spawn "/home/chris/.xmonad/scripts/rotate.sh --left")
+        , ("M-S-<Right>", spawn "/home/chris/.xmonad/scripts/rotate.sh --right")
+        , ("M-S-<Up>", spawn "/home/chris/.xmonad/scripts/rotate.sh --up")
+        , ("M-S-<Down>", spawn "/home/chris/.xmonad/scripts/rotate.sh --down")
+        , ("M-<Up>", windows copyToAll)
+        , ("M-<Down>", killAllOtherCopies)
+        , ("M-<Left>", prevWS)
+        , ("M-<Right>", nextWS)
         ]
